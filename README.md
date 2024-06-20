72e9b3f1269be9fff41129229fc0ea448957c01a
Kaltenstein <dev@mercury-labs.de>
2024-06-20 09:18:11 +0200

    Basic structure

---
+++ README.md
@@ -0,0 +1,20 @@
+## My Personal set of Bitburner Scripts
+
+This repo holds my personal set of Scripts for BitBurner ([Online](https://bitburner-official.github.io)|[Steam](https://store.steampowered.com/app/1812820/Bitburner/)|[GitHub](https://github.com/bitburner-official/bitburner-src))
+
+The base template by [shyguy1412](https://github.com/shyguy1412/) can be found on [Github](https://github.com/shyguy1412/bb-external-editor).
+
+### How to use this
+(Requires arecent-ish version of [nodejs](https://www.nodejs.org))
+
+1. Clone or download repo
+2. Install the packages (`npm i`)
+3. Start the sync process (`npm run start`)
+
+### What is included?
+
+Currently the following features are under development:
+* log-master - A central logging system
+* network-map - A network mapper
+
+Other things are under consideration, development and testing. See the [GitHub Project](https://github.com/users/Nebelherr23/projects/2) for ideas and their stage of planning.
---
+++ build.mjs
@@ -0,0 +1,74 @@
+import { context } from 'esbuild';
+import { BitburnerPlugin } from 'esbuild-bitburner-plugin';
+import chokidar from 'chokidar';
+import fs from 'fs/promises';
+
+/** @type import('esbuild-bitburner-plugin').PluginExtension*/
+const customExtension = {
+  setup() {
+    console.log('Setting up Watcher');
+
+  }, //Run once on plugin startup
+
+  beforeConnect() { }, //Run once before the game connects
+  afterConnect(remoteAPI) {
+    let watcher = chokidar.watch(['./data', './config']);
+    watcher.on('add', path => pushFile(remoteAPI, path));
+    watcher.on('change', path => pushFile(remoteAPI, path)); //Run every time after the game (re)connects
+  },
+  beforeBuild() { }, //Run before every build process
+  afterBuild(remoteAPI) { }, //Run after build, before results are uploaded into the game
+};
+
+async function pushFile(remoteAPI, path) {
+  await fs.stat(path, (err, stat) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+  console.log(`Pushing file ${path}`);
+  
+  let content = await fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  }
+  );
+  console.log(content);
+  console.log(path)
+  return remoteAPI.pushFile({server: 'home', filename: path, content: content}).catch(err => { console.log(err); console.log(`${path} - ${content}`); });
+}
+
+const createContext = async () => await context({
+  entryPoints: [
+    'servers/**/*.js',
+    'servers/**/*.jsx',
+    'servers/**/*.ts',
+    'servers/**/*.tsx',
+  ],
+  outbase: "./servers",
+  outdir: "./build",
+  plugins: [
+    BitburnerPlugin({
+      port: 12525,
+      types: 'NetscriptDefinitions.d.ts',
+      mirror: {
+      },
+      distribute: {
+      },
+      extensions: [
+        customExtension
+      ]
+    })
+  ],
+  bundle: true,
+  format: 'esm',
+  platform: 'browser',
+  logLevel: 'debug'
+});
+
+const ctx = await createContext();
+ctx.watch();
